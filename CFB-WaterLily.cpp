e6025555b54b299d60b104027c3ec82f27a23304
neel1304 <nilarghar@gmail.com>
2019-08-13 15:43:52 +0530

    Create CFB-WaterLily.cpp

---
+++ CFB-WaterLily.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <map>
+#include <set>
+#define FOR(ever) for(int i=0;i<n;i++)
+#define FORE(i,m) for(int i=0;i<m;i++)
+#define W(T) while(T--)
+#define ll long long
+#define pb push_back
+ 
+ 
+//Author: Nilargha Roy (neel13)
+using namespace std;
+int main() {
+ 
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+ 
+#ifdef __APPLE__
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+long long h;
+long long l;
+cin>>h>>l;
+double x=0.0;
+x=(pow(l,2)-pow(h,2))/(2*h);
+cout.precision(8);
+cout<<x;
+return 0;
+}
